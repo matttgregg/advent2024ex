fd2341460bbb05a4e9e4aa67a5434466363993dd
Matthew Gregg <matt.t.gregg@gmail.com>
2024-12-06 19:17:04 +0000

    Inefficient Day6

---
+++ lib/advent2024ex/day6.ex
@@ -0,0 +1,150 @@
+defmodule Advent2024ex.Day6 do
+  def dir_from_char(c) do
+    case c do
+      ?^ -> {-1, 0}
+      ?> -> {0, 1}
+      ?< -> {0, -1}
+      ?v -> {1, 0}
+    end
+  end
+
+  def map_from_file(fname) do
+    File.read!(fname) |> String.split("\n") |> Enum.map(&String.to_charlist/1)
+  end
+
+  defp guard_char(c) do
+    case c do
+      ?^ -> true
+      ?> -> true
+      ?< -> true
+      ?v -> true
+      _ -> false
+    end
+  end
+
+  defp guard_turn(c) do
+    case c do
+      ?^ -> ?>
+      ?> -> ?v
+      ?< -> ?^
+      ?v -> ?<
+    end
+  end
+
+  def find_guard(m) do
+    row = Enum.find_index(m, fn row -> Enum.any?(row, &guard_char/1) end)
+    col = Enum.at(m, row) |> Enum.find_index(&guard_char/1)
+    {row, col}
+  end
+
+  def do_step(m, {grow, gcol}, gchar, steps) do
+    {srow, scol} = dir_from_char(gchar)
+    {nrow, ncol} = {grow + srow, gcol + scol}
+
+    # Is this outside the map? Finish and return the step count.
+    if nrow < 0 || ncol < 0 || nrow >= Enum.count(m) ||
+         ncol >= Enum.at(m, nrow) |> Enum.count() do
+      steps
+    else
+      nchar = Enum.at(m, nrow) |> Enum.at(ncol)
+
+      cond do
+        nchar == ?# ->
+          # This is an obstacle? Turn and try again.
+          do_step(m, {grow, gcol}, guard_turn(gchar), steps)
+
+        nchar == ?X || guard_char(nchar) ->
+          # This a visited square? Move and keep going.
+          do_step(m, {nrow, ncol}, gchar, steps)
+
+        true ->
+          # This an univisited square? Increment, update the map, move and keep going.
+          old_row = Enum.at(m, nrow)
+          new_row = List.replace_at(old_row, ncol, ?X)
+          new_m = List.replace_at(m, nrow, new_row)
+          # IO.puts(Enum.join(new_m, "\n"))
+          do_step(new_m, {nrow, ncol}, gchar, steps + 1)
+      end
+    end
+  end
+
+  def find_path(m, {grow, gcol}, gchar, path) do
+    {srow, scol} = dir_from_char(gchar)
+    {nrow, ncol} = {grow + srow, gcol + scol}
+    npath = Map.put(path, {grow, gcol, gchar}, true)
+
+    # Is this outside the map? Finish and return the step count.
+    if nrow < 0 || ncol < 0 || nrow >= Enum.count(m) ||
+         ncol >= Enum.at(m, nrow) |> Enum.count() do
+      {:left, Map.keys(npath) |> Enum.map(fn {r, c, _c} -> {r, c} end) |> Enum.uniq()}
+    else
+      nchar = Enum.at(m, nrow) |> Enum.at(ncol)
+
+      cond do
+        Map.has_key?(path, {grow, gcol, gchar}) ->
+          # We've looped
+          {:loop}
+
+        nchar == ?# ->
+          # This is an obstacle? Turn and try again.
+          find_path(m, {grow, gcol}, guard_turn(gchar), npath)
+
+        nchar == ?X || guard_char(nchar) ->
+          # This a visited square? Move and keep going.
+          find_path(m, {nrow, ncol}, gchar, npath)
+
+        true ->
+          # This an univisited square? Increment, update the map, move and keep going.
+          old_row = Enum.at(m, nrow)
+          new_row = List.replace_at(old_row, ncol, ?X)
+          new_m = List.replace_at(m, nrow, new_row)
+          # IO.puts(Enum.join(new_m, "\n"))
+          find_path(new_m, {nrow, ncol}, gchar, npath)
+      end
+    end
+  end
+
+  def count_steps(fname) do
+    m = map_from_file(fname)
+    {grow, gcol} = find_guard(m)
+    gchar = Enum.at(m, grow) |> Enum.at(gcol)
+    do_step(m, {grow, gcol}, gchar, 1)
+  end
+
+  def make_loops(fname) do
+    m = map_from_file(fname)
+    {grow, gcol} = find_guard(m)
+    gchar = Enum.at(m, grow) |> Enum.at(gcol)
+    {:left, steps} = find_path(m, {grow, gcol}, gchar, %{})
+
+    # Now, try inserting obstacles at each path point in turn to see if that causes a loop
+    Enum.count(steps, &makes_loop?(m, {grow, gcol}, gchar, &1))
+  end
+
+  defp makes_loop?(m, {grow, gcol}, gchar, {orow, ocol}) do
+    if orow == grow && ocol == gcol do
+      # We can't put an obstacle where the guard is.
+      false
+    else
+      old_row = Enum.at(m, orow)
+      new_row = List.replace_at(old_row, ocol, ?#)
+      new_m = List.replace_at(m, orow, new_row)
+
+      case find_path(new_m, {grow, gcol}, gchar, %{}) do
+        {:left, _} ->
+          false
+
+        {:loop} ->
+          true
+      end
+    end
+  end
+
+  def run(test \\ false) do
+    fname = if test, do: "./puzzle_data/day6.tst", else: "./puzzle_data/day6"
+    part1 = count_steps(fname)
+    part2 = make_loops(fname)
+
+    [part1, part2]
+  end
+end
