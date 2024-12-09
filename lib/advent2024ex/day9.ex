564c76ef7ee1d4495e04900793b0a9b751fc94f2
Matthew Gregg <matt.t.gregg@gmail.com>
2024-12-09 15:30:06 +0000

    Work in progress

--- lib/advent2024ex/day9.ex
+++ lib/advent2024ex/day9.ex
@@ -1,156 +1,200 @@
 defmodule Advent2024ex.Day9 do
-  defstruct running: 0, forward: [], back: [], mode: :data, i: 0, blocks_left: 0, fid: 0
+  defstruct running: 0, forward: [], back: [], i: 0, blocks_left: 0
 
   def load_data(fname) do
+    File.read!(fname) |> parse_data()
+  end
+
+  def parse_data(data) do
     disk_map =
-      File.read!(fname) |> String.trim() |> String.codepoints() |> Enum.map(&String.to_integer/1)
+      data |> String.trim() |> String.codepoints() |> Enum.map(&String.to_integer/1)
 
-    data_only = Enum.drop_every([0 | disk_map], 2)
-    blocks = Enum.sum(data_only)
-
-    back =
-      for i <- 1..Enum.count(data_only) do
-        i - 1
+    forward =
+      for i <- 1..Enum.count(disk_map) do
+        if rem(i, 2) == 0 do
+          # Even indices are spaces
+          {:space, Enum.at(disk_map, i - 1)}
+        else
+          # Odd indices are data
+          {div(i - 1, 2), Enum.at(disk_map, i - 1)}
+        end
       end
-      |> Enum.zip(data_only)
-      |> Enum.reverse()
+
+    data_only = Enum.filter(forward, fn {x, _} -> is_integer(x) end)
+    blocks = Enum.map(data_only, fn {_id, n} -> n end) |> Enum.sum()
+
+    back = Enum.reverse(data_only)
 
     # The number of data blocks is the sum of alternate values.
     %__MODULE__{
       running: 0,
-      forward: disk_map,
+      forward: forward,
       back: back,
       i: 0,
-      blocks_left: blocks,
-      mode: :data,
-      fid: 0
+      blocks_left: blocks
     }
   end
 
-  def step_block(%__MODULE__{blocks_left: 0, running: running}, _) do
+  def step_block(%__MODULE__{blocks_left: 0, running: running}) do
     # We've read all the blocks that we need to. Return the answer.
     running
   end
 
-  def step_block(%__MODULE__{mode: :data, forward: [0 | rest], fid: fid} = acc, contig) do
-    # We're reading a data block, but have run out of data
-    # This means we drop, and shift to space mode.
-    step_block(%__MODULE__{acc | mode: :space, forward: rest, fid: fid + 1}, contig)
+  def step_block(%__MODULE__{forward: [{_, 0} | rest]} = acc) do
+    # We've exhausted a block - move to the next one.
+    step_block(%__MODULE__{acc | forward: rest})
   end
 
-  def step_block(%__MODULE__{mode: :space, forward: [0 | rest]} = acc, contig) do
-    # We're reading a space block, but have run out of space
-    # Flip back to data mode.
-    step_block(%__MODULE__{acc | mode: :data, forward: rest}, contig)
-  end
-
-  def step_block(
-        %__MODULE__{
-          mode: :data,
-          forward: [n | rest],
-          i: i,
-          blocks_left: blocks,
-          running: running,
-          fid: fid
-        } = acc,
-        contig
-      ) do
-    # We're reading data, and have some available. Consume and move on.
-    step_block(
-      %__MODULE__{
-        acc
-        | forward: [n - 1 | rest],
-          i: i + 1,
-          blocks_left: blocks - 1,
-          running: running + i * fid
-      },
-      contig
-    )
-  end
-
-  def step_block(%__MODULE__{mode: :space, back: [{_, 0} | rest]} = acc, contig) do
+  def step_block(%__MODULE__{forward: [{:space, _} | _], back: [{_, 0} | rest]} = acc) do
     # We're reading data from the back, but have run out of data. We move onto the
     # next data block from the back.
-    step_block(%__MODULE__{acc | mode: :space, back: rest}, contig)
+    step_block(%__MODULE__{acc | back: rest})
   end
 
   def step_block(
         %__MODULE__{
-          mode: :space,
-          forward: [fwdn | frest],
+          forward: [{:space, fwdn} | frest],
           back: [{bid, backn} | backrest],
           i: i,
           blocks_left: blocks,
           running: running
-        } = acc,
-        false
+        } = acc
       ) do
     # We're reading data from the back, and have some available
 
-    step_block(
-      %__MODULE__{
-        acc
-        | forward: [fwdn - 1 | frest],
-          back: [{bid, backn - 1} | backrest],
-          i: i + 1,
-          blocks_left: blocks - 1,
-          running: running + i * bid
-      },
-      false
-    )
+    step_block(%__MODULE__{
+      acc
+      | forward: [{:space, fwdn - 1} | frest],
+        back: [{bid, backn - 1} | backrest],
+        i: i + 1,
+        blocks_left: blocks - 1,
+        running: running + i * bid
+    })
+  end
+
+  def step_block(%__MODULE__{forward: [{fid, 0} | rest]} = acc)
+      when is_integer(fid) do
+    # We're reading a data block, but have run out of data
+    # This means we move to the next block.
+    step_block(%__MODULE__{acc | forward: rest})
   end
 
   def step_block(
         %__MODULE__{
-          mode: :space,
-          forward: [fwdn | frest],
-          back: back,
+          forward: [{fid, n} | rest],
           i: i,
           blocks_left: blocks,
           running: running
-        } = acc,
-        true
-      ) do
-    # We're reading data from the back, have some, but only allow full blocks.
-
-    # Find which block we can use.
-    block_index = Enum.find_index(back, fn {_, size} -> size <= fwdn end)
-
-    if block_index do
-      {bid, size} = Enum.at(back, block_index)
-      IO.puts("Fitting #{bid} length #{size} at #{i}")
-      increment = bid * div((i + (i + size - 1)) * size, 2)
-
-      step_block(
-        %__MODULE__{
-          acc
-          | forward: [fwdn - size | frest],
-            back: List.delete_at(back, block_index),
-            i: i + size,
-            blocks_left: blocks - size,
-            running: running + increment
-        },
-        true
+        } = acc
       )
-    else
-      # There is no block that will fit. Just skip the rest of this space and read data again.
-      step_block(
-        %__MODULE__{
-          acc
-          | forward: frest,
-            i: i + fwdn,
-            mode: :data
-        },
-        true
-      )
+      when is_integer(fid) do
+    # We're reading data, and have some available. Consume and move on.
+    step_block(%__MODULE__{
+      acc
+      | forward: [{fid, n - 1} | rest],
+        i: i + 1,
+        blocks_left: blocks - 1,
+        running: running + i * fid
+    })
+  end
+
+  defp fits({:space, n}, {_id, size}) do
+    size <= n
+  end
+
+  defp fits(_, _), do: false
+
+  def compress_map(dm, []) do
+    dm
+  end
+
+  def compress_map(dm, [to_fit | rst]) do
+    move_from = Enum.find_index(dm, fn x -> x == to_fit end)
+
+    case to_fit do
+      {:space, _} ->
+        # Don't try to move spaces
+        compress_map(dm, rst)
+
+      {_id, size} = el ->
+        # See if there's a space that can take it
+        if (move_to = Enum.find_index(dm, &fits(&1, el))) && move_to < move_from do
+          dm_with_space =
+            case {Enum.at(dm, move_from - 1), Enum.at(dm, move_from + 1)} do
+              {{:space, sa}, {:space, sb}} ->
+                List.replace_at(dm, move_from - 1, {:space, size + sa + sb})
+                |> List.delete_at(move_from)
+                |> List.delete_at(move_from)
+
+              {{:space, sn}, _} ->
+                List.replace_at(dm, move_from - 1, {:space, sn + size})
+                |> List.delete_at(move_from)
+
+              {_, {:space, sn}} ->
+                List.replace_at(dm, move_from + 1, {:space, sn + size})
+                |> List.delete_at(move_from)
+
+              _ ->
+                List.replace_at(dm, move_from, {:space, size})
+            end
+
+          {:space, gap_size} = Enum.at(dm, move_to)
+
+          new_dm =
+            if gap_size > size do
+              # The existing space is shrunk
+              dm_with_space
+              |> List.replace_at(move_to, {:space, gap_size - size})
+              |> List.insert_at(move_to, el)
+            else
+              # The space is completely replaced.
+              dm_with_space
+              |> List.replace_at(move_to, el)
+            end
+
+          compress_map(new_dm, rst)
+        else
+          # We can't move this element - just leave it on the end and keep going.
+          compress_map(dm, rst)
+        end
     end
+  end
+
+  defp string_for_el({:space, n}), do: String.duplicate(".", n)
+  defp string_for_el({id, n}), do: String.duplicate("#{id}", n)
+
+  def debug_dmap(dm) do
+    Enum.map(dm, &string_for_el/1) |> Enum.join()
+  end
+
+  def load_and_compress_from_file(fname) do
+    File.read!(fname) |> load_and_compress()
+  end
+
+  def load_and_compress(data) do
+    %__MODULE__{forward: forward, back: back} = parse_data(data)
+    compress_map(forward, back)
+  end
+
+  def evaluate_dm([], _, acc), do: acc
+
+  def evaluate_dm([{idx, size} | rest], i, acc) when is_integer(idx) do
+    increment = idx * div(size * (2 * i + size - 1), 2)
+    evaluate_dm(rest, i + size, acc + increment)
+  end
+
+  def evaluate_dm([{:space, n} | rest], i, acc) do
+    evaluate_dm(rest, i + n, acc)
+  end
+
+  def test2(data) do
+    load_and_compress(data) |> debug_dmap()
   end
 
   def run(test \\ false) do
     fname = if test, do: "./puzzle_data/day9.tst", else: "./puzzle_data/day9"
-    disk_map = load_data(fname)
-    part1 = step_block(disk_map, false)
-    part2 = step_block(disk_map, true)
+    part1 = load_data(fname) |> step_block()
+    part2 = load_and_compress_from_file(fname) |> evaluate_dm(0, 0)
 
     [part1, part2]
   end
